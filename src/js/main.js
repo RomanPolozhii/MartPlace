f034873ccdf93f5f6ea33158fddf925ab8ae980b
RomanPolozhii <roman.polozhii@gmail.com>
2020-05-04 19:19:05 +0300

    adeed

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,17 @@
 $(function () {
+  function ibg() {
+    $.each($(".ibg"), function (index, val) {
+      if ($(this).find("img").length > 0) {
+        $(this).css(
+          "background-image",
+          'url("' + $(this).find("img").attr("src") + '")'
+        );
+      }
+    });
+  }
+
+  ibg();
+
   $(".user__btn").click(function () {
     $(".user__btn,.mobile-list, .lnr-cross").toggleClass("active");
   });
