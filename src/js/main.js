4cb263041d4148df4145930e1ff039ba33e772aa
Roman <Roman@MacBook-Pro.local>
2020-05-09 14:29:59 +0300

    services__seartch

--- src/js/main.js
+++ src/js/main.js
@@ -78,12 +78,15 @@ $(function () {
     readOnly: true,
     ratedFill: "#ffc000",
     starSvg:
-      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path stroke="#ffc000" d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>'
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path stroke="#ffc000" d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>',
   });
   $(".slider-feature").slick({
-    appendArrows:$(".feature__title")
+    appendArrows: $(".feature__title"),
   });
 
-  $('input, select').styler();
+  $("input, select").styler();
 
+  $(".jq-selectbox__select").click(function () {
+    $(".jq-selectbox__select").toggleClass("opened");
+  });
 });
